5cac6f2568339e6912c57fd98bc9d36e5b0985d5
Massiccio1 <girardellimassimo@gmail.com>
2024-03-25 18:44:22 +0100

    update

--- commander/entrypoint.sh
+++ commander/entrypoint.sh
@@ -9,5 +9,5 @@ function ctrl_c() {
 rm /tmp/.X1-lock 2> /dev/null &
 /opt/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
 # Insecure option is needed to accept connections from the docker host.
-vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE &
+novnc $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE &
 wait
