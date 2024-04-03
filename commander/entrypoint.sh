236eca733771c706ebbd9f8dbe9d44abaaa55a05
Massiccio1 <girardellimassimo@gmail.com>
2024-04-03 14:22:47 +0200

    fix some typos

--- commander/entrypoint.sh
+++ commander/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 # set -e: exit asap if a command exits with a non-zero status
-set -e
-trap ctrl_c INT
-function ctrl_c() {
-  exit 0
-}
-# entrypoint.sh file for starting the xvfb with better screen resolution, configuring and running the vnc server.
-rm /tmp/.X1-lock 2> /dev/null &
-/opt/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
-# Insecure option is needed to accept connections from the docker host.
-novnc $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE &
-wait
+cd /root
+source ~/.bashrc ; source /opt/ros/humble/setup.bash ; source ~/ros2_ws/install/setup.bash ; true
+cd ~/ros2_ws/src/px4-py/src && python3 px4-py.py &
+cd  ~/ros2_ws/src/px4-py/src && python3 gui.py &
+cd ~
+bash
